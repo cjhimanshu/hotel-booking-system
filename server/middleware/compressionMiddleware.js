8a54fb66a83c0d8027bc9e6f030fdd86816bf89a
cj_ <cjhimanshu49@gmail.com>
2026-04-13 23:16:18 +0530

    perf: add response compression middleware

---
+++ server/middleware/compressionMiddleware.js
@@ -0,0 +1,14 @@
+// Compression middleware for reducing response size
+const compression = require('compression');
+
+const compressionMiddleware = compression({
+  filter: (req, res) => {
+    if (req.headers['x-no-compression']) {
+      return false;
+    }
+    return compression.filter(req, res);
+  },
+  level: 6, // compression level 1-9
+});
+
+module.exports = compressionMiddleware;
