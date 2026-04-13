03aa1910bc94c05633765069a961ecca31121e7b
cj_ <cjhimanshu49@gmail.com>
2026-04-13 23:14:05 +0530

    utils: add standardized API response formatter

---
+++ server/utils/responseFormatter.js
@@ -0,0 +1,21 @@
+// Standardized API response formatter
+const sendSuccess = (res, statusCode, message, data = null) => {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+  });
+};
+
+const sendError = (res, statusCode, message, errors = null) => {
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errors,
+  });
+};
+
+module.exports = {
+  sendSuccess,
+  sendError,
+};
